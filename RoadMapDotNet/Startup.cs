5cde70b37a0b83be1a142e5fb500a4f08df2fc7e
Daniel Li <neolee11@yahoo.com>
2015-12-17 23:01:47 -0500

    Init commit

---
+++ RoadMapDotNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RoadMapDotNet.Startup))]
+namespace RoadMapDotNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
