c639b54e3db42b2205c15ae2b7b389bc47416cc3
Daniel Li <neolee11@yahoo.com>
2015-12-20 16:42:20 -0500

    Added some tests

---
+++ RoadMapDotNet.Test/Code/CSharp6FeatureTests.cs
@@ -0,0 +1,24 @@
+﻿using RoadMapDotNet.Code;
+using Xunit;
+
+namespace RoadMapDotNet.Test.Code
+{
+    public class CSharp6FeatureTests
+    {
+        [Fact]
+        public void UsingStaticShouldWork()
+        {
+            var sut = new CSharp6Features();
+            var result = sut.UsingStaticAbs(-3);
+            Assert.Equal(result, 3);
+        }
+
+        [Fact]
+        public void UsingAutoPropertyShouldWork()
+        {
+            var sut = new CSharp6Features();
+            var result = sut.UsingAutoPropertyInitializer();
+            Assert.Equal(result, 10);
+        }
+    }
+}
---
+++ RoadMapDotNet/Code/CSharp6Features.cs
@@ -0,0 +1,20 @@
+﻿using static System.Math;
+
+namespace RoadMapDotNet.Code
+{
+    public class CSharp6Features
+    {
+        public int Num { get; set; } = 10;
+        public int Something { get; set; }
+
+        public int UsingStaticAbs(int i)
+        {
+            return Abs(i);
+        }
+
+        public int UsingAutoPropertyInitializer()
+        {
+            return Num;
+        }
+    }
+}
