8c1b5737825f7c0950d5fca5eb65f7a5483d8998
Daniel Li <neolee11@yahoo.com>
2015-12-27 23:29:04 -0500

    Added basic structure

---
+++ RoadMapDotNet.Core.Tests/ExampleTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RoadMapDotNet.Core.Tests
+{
+    public class ExampleTest
+    {
+        [Fact]
+        public void TestSomething()
+        {
+            Assert.Equal(3, 3);
+        }
+    }
+}
